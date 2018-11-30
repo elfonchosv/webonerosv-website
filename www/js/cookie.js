f8412867261b7ba3651b32bacbd4c42d91ec5258
elfonchosv <44900457+elfonchosv@users.noreply.github.com>
2018-11-30 10:09:39 -0600

    Initial commit

---
+++ www/js/cookie.js
@@ -0,0 +1,19 @@
+<script>
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "position": "bottom-left",
+  "content": {
+    "message": "Utilizamos cookies para mejorar la experiencia del usuario",
+    "dismiss": "Entendido",
+    "link": "Leer más"
+  }
+})});
+</script>
---
+++ www/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.tap-target').tapTarget();
+  $('.modal').modal();
+  $(".dropdown-trigger").dropdown({
+   //hover: true
+  });
+  $('.sidenav').sidenav();
+});
